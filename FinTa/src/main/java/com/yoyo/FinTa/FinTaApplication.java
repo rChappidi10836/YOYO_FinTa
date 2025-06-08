c0c1cc13f78ae2dc1ebc30e7c2135853092f4132
rChappidi10836 <rajasrinivas1596@gmail.com>
2025-06-08 15:12:19 +0530

    FinTa, project first push

---
+++ FinTa/src/main/java/com/yoyo/FinTa/FinTaApplication.java
@@ -0,0 +1,15 @@
+package com.yoyo.FinTa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinTaApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Yes working");
+		SpringApplication.run(FinTaApplication.class, args);
+
+	}
+
+}
---
+++ FinTa/src/test/java/com/yoyo/FinTa/FinTaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoyo.FinTa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinTaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

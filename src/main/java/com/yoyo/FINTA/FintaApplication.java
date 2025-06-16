88f9ac41c07938e60cf2fbf80a358f0573558095
rChappidi10836 <rajasrinivas1596@gmail.com>
2025-06-16 06:20:54 +0530

    FinTa, project first push

---
+++ src/main/java/com/yoyo/FINTA/FintaApplication.java
@@ -0,0 +1,13 @@
+package com.yoyo.FINTA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintaApplication.class, args);
+	}
+
+}
